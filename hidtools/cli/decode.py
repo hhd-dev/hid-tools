hhd-dev/hid-tools

commit 6de3877487ca62df9e212bb0ffbb1db0ca311cdc

decode: recognize a human-readable report descriptor
This is the format that hid-record spits out as part of the comments, e.g.

	Usage Page (Power Device)           0
	Usage (Vendor Usage 0x04)           2
	Collection (Application)            4
	 Usage (Vendor Usage 0x24)          6
	 Collection (Physical)              8
	  Usage (Vendor Usage 0x02)         10

We just search for at least one line with 'Usage Page (', if it exists then
we pass it to the parser.

Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

hidtools/cli/decode.py
@@ -75,14 +75,20 @@ def open_binary(path):
 
 
 def interpret_file_hidrecorder(fd):
-    r_lines = [l for l in fd if l.startswith('R: ')]
-    if not r_lines:
-        return None
-
     rdescs = []
-    for l in r_lines:
-        bytes = l[3:]  # drop R:
-        rdescs.append(hidtools.hid.ReportDescriptor.from_string(bytes))
+    data = fd.read()
+    # The proper (machine-readable) hid-recorder output
+    r_lines = [l for l in data.splitlines() if l.startswith('R: ')]
+    if r_lines:
+        for l in r_lines:
+            bytes = l[3:]  # drop R:
+            rdescs.append(hidtools.hid.ReportDescriptor.from_string(bytes))
+
+    # The human-readable version (the comment in the hid-recorder output)
+    if any(filter(lambda x: x.strip().startswith('Usage Page ('), data.splitlines())):
+        rdescs = [hidtools.hid.ReportDescriptor.from_human_descr(data)]
+        if not rdescs:
+            return None
 
     return rdescs
 

commit 7f226fcf4e057ce30142a91e8b04edc86bb3cac9

decode: fix human descriptor detection
Where the file wasn't a human-readable descriptor, we'd end up returning an
empty list - and that is not None. So we ended up never detecting the format.

Regression introduced in 6de3877487ca62df9e212bb0ffbb1db0ca311cdc

Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

hidtools/cli/decode.py
@@ -75,22 +75,24 @@ def open_binary(path):
 
 
 def interpret_file_hidrecorder(fd):
-    rdescs = []
     data = fd.read()
     # The proper (machine-readable) hid-recorder output
     r_lines = [l for l in data.splitlines() if l.startswith('R: ')]
     if r_lines:
+        rdescs = []
         for l in r_lines:
             bytes = l[3:]  # drop R:
             rdescs.append(hidtools.hid.ReportDescriptor.from_string(bytes))
+        if rdescs:
+            return rdescs
 
     # The human-readable version (the comment in the hid-recorder output)
     if any(filter(lambda x: x.strip().startswith('Usage Page ('), data.splitlines())):
         rdescs = [hidtools.hid.ReportDescriptor.from_human_descr(data)]
-        if not rdescs:
-            return None
+        if rdescs:
+            return rdescs
 
-    return rdescs
+    return None
 
 
 def interpret_file_libinput_record(fd):