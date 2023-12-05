3fa5190cdb2c17397b0fafa62e485e01d920b3cc
Benjamin Tissoires <benjamin.tissoires@gmail.com>
2023-12-05 18:09:19 +0100

    doc: fix mypy complains
    doc/source/conf.py:75: error: Need type annotation for "exclude_patterns" (hint: "exclude_patterns: List[<type>] = ...")  [var-annotated]
    doc/source/conf.py:120: error: Need type annotation for "latex_elements" (hint: "latex_elements: Dict[<type>, <type>] = ...")  [var-annotated]
    
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@gmail.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -17,6 +17,8 @@
 import os
 import sys
 
+from typing import Dict, List
+
 sys.path.insert(0, os.path.abspath("../../"))
 sys.path.insert(0, os.path.abspath("../../hidtools"))
 sys.path.insert(0, os.path.abspath("../../tests"))
@@ -72,7 +74,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = []
+exclude_patterns: List[str] = []
 
 add_module_names = False
 
@@ -117,7 +119,7 @@ htmlhelp_basename = "hid-toolsdoc"
 
 # -- Options for LaTeX output ------------------------------------------------
 
-latex_elements = {
+latex_elements: Dict[str, str] = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
